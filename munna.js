de073ee4094531b05ccd1735f61e877de585a07b
lazy-coder420 <mjmunna692@gmail.com>
2026-03-12 21:47:41 +0600

    new

---
+++ munna.js
@@ -0,0 +1,17 @@
+// munna@Munnas-MacBook-Air backend % git add  .
+// munna@Munnas-MacBook-Air backend % git commit -m"part1"
+// [main 0d93d0e] part1
+//  2 files changed, 107 insertions(+)
+//  create mode 100644 munna.js
+//  create mode 100644 test1/test2API/apl.js
+// munna@Munnas-MacBook-Air backend % git push 
+// Enumerating objects: 7, done.
+// Counting objects: 100% (7/7), done.
+// Delta compression using up to 10 threads
+// Compressing objects: 100% (4/4), done.
+// Writing objects: 100% (5/5), 628 bytes | 628.00 KiB/s, done.
+// Total 5 (delta 1), reused 0 (delta 0), pack-reused 0 (from 0)
+// remote: Resolving deltas: 100% (1/1), completed with 1 local object.
+// To https://github.com/lazy-coder420/backend.git
+//    8db9536..0d93d0e  main -> main
+// munna@Munnas-MacBook-Air backend % 
